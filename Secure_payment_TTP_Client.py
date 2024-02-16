f4f0bd04b919279de0ae4b7b7ce922e9f6b18273
Ilie-Cristi <61991016+Ilie-Cristi@users.noreply.github.com>
2024-02-16 13:26:48 +0200

    Add files via upload
    Clean execution of the protocol described in the challenge

---
+++ Secure_payment_TTP_Client.py
@@ -0,0 +1,183 @@
+from random import randint
+from netqasm.sdk import Qubit
+from netqasm.sdk.classical_communication.message import StructuredMessage
+import hmac
+import sqlite3
+import hashlib
+from squidasm.sim.stack.common import LogManager
+from squidasm.sim.stack.program import Program, ProgramContext, ProgramMeta
+
+
+class TTPProgram(Program):
+    PEER_NAME = "Client"
+
+    def __init__(self, num_qubits):
+        self.logger = LogManager.get_stack_logger(self.__class__.__name__)
+
+        #The number of teleported qubits 
+        self._num_teleported_qubits = num_qubits
+
+
+    @property
+    def meta(self) -> ProgramMeta:
+        return ProgramMeta(
+            name="controller_program",
+            csockets=[self.PEER_NAME],
+            epr_sockets=[self.PEER_NAME],
+            max_qubits=self._num_teleported_qubits,
+        )
+
+    def run(self, context: ProgramContext):
+        csocket = context.csockets[self.PEER_NAME]
+        epr_socket = context.epr_sockets[self.PEER_NAME]
+        connection = context.connection
+
+        b = [randint(0,1) for _ in range(self._num_teleported_qubits)]
+        B = [randint(0,1) for _ in range(self._num_teleported_qubits)]
+
+        print(f'Bitstream:{b}\nBasis:    {B}')
+
+        for i in range(self._num_teleported_qubits):
+            q = Qubit(connection)
+            if b[i] == 1:
+                q.X()
+            if B[i] == 0:
+                q.H()
+
+            # Create EPR pairs
+            epr = epr_socket.create_keep()[0]
+            # Teleport
+            q.cnot(epr)
+            q.H()
+            m1 = q.measure()
+            m2 = epr.measure()
+            yield from connection.flush()
+            # Send the correction information
+            m1, m2 = int(m1), int(m2)
+
+            self.logger.info(
+                f"Performed teleportation protocol with measured corrections: m1 = {m1}, m2 = {m2}"
+            )
+
+            csocket.send_structured(StructuredMessage("Corrections", f"{m1},{m2}"))
+
+        
+        return b,B
+
+
+class ClientProgram(Program):
+    PEER_NAME = "TTP"
+
+    def __init__(self, num_qubits):
+        self.logger = LogManager.get_stack_logger(self.__class__.__name__)
+        self._num_teleported_qubits = num_qubits
+
+    @property
+    def meta(self) -> ProgramMeta:
+        return ProgramMeta(
+            name="controller_program",
+            csockets=[self.PEER_NAME],
+            epr_sockets=[self.PEER_NAME],
+            max_qubits=self._num_teleported_qubits,
+        )
+
+    def run(self, context: ProgramContext):
+        csocket = context.csockets[self.PEER_NAME]
+        epr_socket = context.epr_sockets[self.PEER_NAME]
+        connection = context.connection
+
+        # Define the client's secret C
+        C = 'TopSecretC'.encode()
+        
+        # Use the merchant identification
+        M = 'MerchantID'.encode()
+
+        # Create the hmac based on the number of qubits sent (insecure implementation)
+        fullmac = hmac.digest(C,M,'sha512')
+        n = self._num_teleported_qubits
+        if n%8 != 0:
+            m = fullmac[:n//8]+(fullmac[n//8+1]>>(8-n%8)).to_bytes(1,'big')
+        else:
+            m = fullmac[:n//8]
+
+        measured_basis = []
+
+
+        k = []
+        for i in range(n):
+            epr = epr_socket.recv_keep()[0]
+            yield from connection.flush()
+            self.logger.info("Created EPR pair")
+
+
+            # Get the corrections
+            msg = yield from csocket.recv_structured()
+            print(msg)
+            assert isinstance(msg, StructuredMessage)
+            m1, m2 = msg.payload.split(",")
+            self.logger.info(f"Received corrections: {m1}, {m2}")
+            if int(m2) == 1:
+                self.logger.info("performing X correction")
+                epr.X()
+            if int(m1) == 1:
+                self.logger.info("performing Z correction")
+                epr.Z()
+
+            # Define the measurement basis
+            if (m[i // 8] >> (8 - i % 8)) % 2 == 0:
+                epr.H()
+
+            measured_basis.append((m[i // 8] >> (8 - i % 8)) % 2)
+            em = epr.measure()
+            yield from connection.flush()
+            k.append(int(em))
+            
+        
+        print(f'Client bitstream: {k}\nClient basis:     {measured_basis}')
+        return k, hashlib.md5(C).digest()
+
+#Define the TTP entity
+class TTP():
+    def __init__(self,n,b,B,k,M,CID):
+        self.MerchanID = M
+        self.ClientKey = k
+        self.ClientID = CID
+        self._num_teleported_qubits = n
+        self.initial_bitstream = b
+        self.initial_basis = B
+        self.BER = 0
+
+    def verify_P(self):
+        """
+        This method will return the bit error rate for the qbits measured in the same basis
+        """
+        if self.ClientID == 'TopSecretC'.encode():
+            C = 'TopSecretC'.encode()
+            if self.check_client:
+                fullmac = hmac.digest(C,self.MerchanID,'sha512')
+                n = self._num_teleported_qubits
+                if n % 8 != 0:
+                    m = fullmac[:n//8]+(fullmac[n//8+1]>>(8-n%8)).to_bytes(1,'big')
+                else:
+                    m = fullmac[:n//8]
+            self.mismatches = 0
+            for i in range(n):
+                if self.initial_basis[i] == m[i] and self.ClientKey[i] != self.initial_bitstream[i]:
+                    self.mismatches += 1
+
+            self.BER = self.mismatches / n
+
+        return self.BER
+
+
+
+# Define the merchant entity
+class Merchant():
+    def __init__(self,k,CID):
+        self.key = k
+        self.merchantID = 'MerchantID'.encode()
+        self.clientID = CID
+
+    def return_to_bank(self):
+        return self.key, self.merchantID, self.clientID
+    
---
+++ Secure_payment_run.py
@@ -0,0 +1,55 @@
+from Secure_payment_TTP_Client import TTPProgram, ClientProgram, TTP, Merchant
+
+import numpy as np
+import logging
+from squidasm.run.stack.run import run
+from squidasm.sim.stack.common import LogManager
+from squidasm.run.stack.config import StackNetworkConfig
+
+
+# Create a network configuration
+cfg = StackNetworkConfig.from_file("Secure_payment.yaml")
+
+# Define the number of qubits used for |P>
+num_qubits = 10 
+
+# Create instances of programs to run
+Client_program = ClientProgram(num_qubits)
+TTP_program = TTPProgram(num_qubits)
+
+# toggle logging. Set to logging.INFO for logging of events.
+Client_program.logger.setLevel(logging.ERROR)
+TTP_program.logger.setLevel(logging.ERROR)
+
+# Run the simulation. Programs argument is a mapping 
+# of network node labels to programs to run on that node
+# As we cannot make use of three channels, we return 
+# the needed information for later use
+Client_step1, TTP_step1  = run(
+    config=cfg,
+    programs={"TTP": TTP_program, "Client": Client_program},
+    num_times=1,
+)
+
+# Keep the initial bitstream and the initial basis
+b = TTP_step1[0][0]
+B = TTP_step1[0][1]
+
+# Keep the key and the client ID in order to send them to the merchant
+k = Client_step1[0][0]
+CID = Client_step1[0][1]
+
+# Send the key and CID to the Merchant 1
+Merchant1 = Merchant(k,CID)
+
+# The key, merchant ID and client ID sent from merchant to bank
+k_r = Merchant1.return_to_bank()[0]
+M_r = Merchant1.return_to_bank()[1]
+CID_r = Merchant1.return_to_bank()[2]
+
+
+# Create a TTP instance named Bank and send the key, CID and merchant ID
+# The initial bitstream and the initial basis vector were kept in memory
+# to verify the validity of the transaction
+Bank = TTP(num_qubits, b, B, k_r, M_r, CID_r)
+print(Bank.verify_P())
